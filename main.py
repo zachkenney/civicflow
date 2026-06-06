18dc8dd01fd2a9016c68e8f68d61c558bae248ce
zachkenney <zachkenney.zk@gmail.com>
2026-06-06 10:20:47 -0400

    Added scheduler to main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from extract import *
 from load import *
+import schedule
+import time
 
-cursor = connect()
-setup(cursor)
-data = getData(cursor)
-load(cursor,data)
+def run():
+    cursor = connect()
+    setup(cursor)
+    data = getData(cursor)
+    load(cursor,data)
+
+schedule.every(24).hours.do(run)
+
+run()
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
