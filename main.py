59ea0116da547b6f0f89a2403b5656fc754a5b5d
zachkenney <zachkenney.zk@gmail.com>
2026-06-12 14:45:23 -0400

    Added the transform layer to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from extract import *
 from load import *
+from transform import *
 import schedule
 import time
 from db import connect, conn
@@ -9,6 +10,8 @@ def run():
     setup(cursor)
     data = getData(cursor)
     load(cursor,data)
+    silver_setup(cursor)
+    silver_load()
 
 schedule.every(24).hours.do(run)
 
